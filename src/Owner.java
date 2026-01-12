f1ea6ca3b61fc1c69d3ca5a3c11df2a074d3896c
Callum Moore <callum@cantonim.me>
2026-01-12 22:56:11 +0000

    Fixing an accidentally left in debug comment

--- src/Owner.java
+++ src/Owner.java
@@ -89,7 +89,6 @@ public class Owner {
 		}
 
 		int length = size();
-		System.out.println(length);
 		for (int i = 0; i < length; i++) {
 				if (this != dog.getOwner()) {
 					dog.setOwner(this);
