54e5feac309e3f3ce024c1debc7f70c61388d60c
Callum Moore <callum@cantonim.me>
2026-01-12 23:02:29 +0000

    Fixing a small problem with the insertion of dogs and optimizing ownsDogs with a name.

--- src/Owner.java
+++ src/Owner.java
@@ -88,17 +88,13 @@ public class Owner {
 			return false;
 		}
 
-		int length = size();
-		for (int i = 0; i < length; i++) {
-				if (this != dog.getOwner()) {
-					dog.setOwner(this);
-				}
-				
-				this.dogs[i] = dog;
-				return true;
+		if (this != dog.getOwner()) {
+			dog.setOwner(this);
 		}
+		
+		this.dogs[size()] = dog;
+		return true;
 
-		return false;
 	}
 
 	private boolean weOwn(Dog dog) {
@@ -162,7 +158,8 @@ public class Owner {
 	}
 
 	public boolean ownsDog(Dog dog) {
-		for (int i = 0; i < dogs.length; i++) {
+		int length = size();
+		for (int i = 0; i < length; i++) {
 			if (dogs[i] == dog) {
 				return true;
 			}
